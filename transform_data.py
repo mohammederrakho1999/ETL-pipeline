915a3fdd767169327c19fe21c32f44220abdda8a
mohammederrakho <65449917+mohammederrakho1999@users.noreply.github.com>
2021-01-12 16:43:50 +0100

    Update transform_data.py

--- transform_data.py
+++ transform_data.py
@@ -9,7 +9,7 @@ import logging
 config = configparser.ConfigParser()
 config.read_file(open(f"{Path(__file__).parents[0]}/config.cfg"))
 
-class OlistTransform:
+class Transform_data:
 	"""This class performs transformations operations on the diffrent datasets"""
 
     def __init__(self, spark):
@@ -31,8 +31,9 @@ class OlistTransform:
         books = books.withColumn("Publisher", fn.regexp_replace("Publisher","N/A","other"))
         
 	logging.debug("writing books data")
-        books.repartition(2).write \
-                                .csv(path = self._save_path + "/books/", mode = "overwrite", compression = "gzip", header = True)
+        books.write \
+             .csv(path = self._save_path + "/books/", mode = "overwrite", compression = "gzip", header = True)
+	
                         
     def transform_user_data(self):
 	logging.debug("inside transform user data")
@@ -57,6 +58,7 @@ class OlistTransform:
 
     def transform_book_ratings_data(self):
 	logging.debug("inside trasform book ratings data")
+	
 	book_ratings = self._spark.read.format("csv") \ 
 	                    .option("header","true") \
 	                    .option("sep",";")\ 
@@ -67,7 +69,7 @@ class OlistTransform:
 	                    .option("header","true") \
 	                    .option("sep",";")\ 
 	                    .option("inferShema","true") \
-	                    .load(self._load_path, + "/books/.csv")
+	                    .load(self._load_path, + "/books.csv/")
 	
 	
 	book_ratings  = book_ratings.where(book_ratings['ISBN'].isin(books.select('ISBN').distinct().rdd.flatMap(lambda x:x).collect()[0]))
@@ -76,36 +78,3 @@ class OlistTransform:
 	book_ratings.repartition(2).write \
                                    .csv(path = self._save_path + "/book_ratings/", mode = "overwrite", compression = "gzip", header = True)
 	
-
-
-	df_customer_data = df_customers.filter(df_customers.customer_id != "NaN") \
-		                               .select(["customer_id","customer_id","customer_state"])
-
-	df_order_data = df_order.join(df_order_items, on = ["order_id"], how = "inner") \
-		                        .filter(df_order.customer_id !="NaN") \
-		                        .select(["customer_id","order_id","order_status","order_delivered_customer_date","product_id"])
-	
-
-        df_order_data.persist()
-        fn.broadcast(df_order_data)
-
-        df_product_data = df_order_data.join(df_product, on=["product_id"], how = "inner") \
-                                               .filter(df_product.product_category_name !="NaN") \
-                                               .select(["product_id","product_category_name","product_name_lenght","product_description_lenght"])
-
-
-        logging.debug("repartitiong data and saving it")
-
-        df_customer_data.repartition(2).write \
-                                               .csv(path = self._save_path + "/customers/", mode = "overwrite", compression = "gzip", header = True)
-        df_order_data.repartition(2).write \
-                                            .csv(path = self._save_path + "/order/", mode = "overwrite", compression = "gzip", header = True)
-        df_product_data.repartition(2).write \
-                                              .csv(path = self._save_path + "/products/", mode = "overwrite", compression = "gzip", header = True)
-
-
-
-
-
-
-
